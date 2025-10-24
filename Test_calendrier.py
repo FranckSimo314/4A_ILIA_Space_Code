3534c61f6774d9014acc4c209a19cd84a7777637
FranckSimo314 <dzessifranck@gmail.com>
2025-10-24 13:18:27 +0000

    test: Ordre_chronologique

--- Test_calendrier.py
+++ Test_calendrier.py
@@ -8,3 +8,11 @@ def test_creer_evenement():
     assert e in evenements
     assert e[2] == "Test"
 
+def test_evenements_chronologiques():
+    evenements.clear()
+    T1 = time.time()
+    creer_evenement(T1, 20, "B")
+    creer_evenement(T1, 10, "A")
+    triés = evenements_chronologiques()
+    assert triés[0][2] == "A"
+    assert triés[1][2] == "B"
