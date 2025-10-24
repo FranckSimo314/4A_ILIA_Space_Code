d9cee950a5b7d9461efaaf572eeec0ec7e041509
FranckSimo314 <dzessifranck@gmail.com>
2025-10-24 13:28:55 +0000

    Test: Ajout du reste des fonctions test

--- Test_calendrier.py
+++ Test_calendrier.py
@@ -16,10 +16,18 @@ def test_evenements_chronologiques():
     triés = evenements_chronologiques()
     assert triés[0][2] == "A"
     assert triés[1][2] == "B"
-    
+
 def test_premier_evenement():
     evenements.clear()
     T1 = time.time()
     creer_evenement(T1, 5, "Premier")
     creer_evenement(T1, 10, "Deuxième")
     assert premier_evenement() == "Premier"
+
+def test_prochain_evenement():
+    evenements.clear()
+    T1 = time.time()
+    creer_evenement(T1, 1, "Prochain")
+    time.sleep(1.1)  # attend que l'événement soit passé
+    creer_evenement(T1, 5, "Plus tard")
+    assert prochain_evenement() == "Plus tard"
